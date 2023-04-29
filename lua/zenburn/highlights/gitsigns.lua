f4b482e8ae93b30d1fe556fc17aaf2e90fe596d9
Joseph Martin <joseph@josephmartin.org>
2023-04-29 00:56:51 -0400

    Update sign column colors

--- lua/zenburn/highlights/gitsigns.lua
+++ lua/zenburn/highlights/gitsigns.lua
@@ -1,12 +1,12 @@
 local c = require("zenburn.palette")
 return {
-    GitSignsAdd = { fg=c.Comment.fg },
+    GitSignsAdd = { fg=c.Comment.fg, bg=c.SignColumn.bg },
     GitSignsAddNr = { fg=c.Comment.fg },
     GitSignsAddLn = { bg=c.DiffAdd.bg },
-    GitSignsChange = { fg=c.Label.fg },
+    GitSignsChange = { fg=c.Label.fg, bg=c.SignColumn.bg },
     GitSignsChangeNr = { fg=c.Label.fg },
     GitSignsChangeLn = { bg=c.DiffChange.bg },
-    GitSignsDelete = { fg=c.String.fg },
+    GitSignsDelete = { fg=c.String.fg, bg=c.SignColumn.bg },
     GitSignsDeleteNr = { fg=c.String.fg },
     GitSignsDeleteLn = { bg=c.DiffText.bg },
 }
